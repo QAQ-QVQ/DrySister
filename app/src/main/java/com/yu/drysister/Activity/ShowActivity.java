eda4fd758fceb27090facf08db3e64bf60ded3cb
QAQ_QVQ <727005920@qq.com>
2019-07-03 14:27:42 +0800

    添加返回更多按钮

--- app/src/main/java/com/yu/drysister/Activity/ShowActivity.java
+++ app/src/main/java/com/yu/drysister/Activity/ShowActivity.java
@@ -2,13 +2,18 @@ package com.yu.drysister.Activity;
 
 import android.graphics.drawable.Drawable;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
@@ -34,30 +39,42 @@ public class ShowActivity extends BaseActivity implements View.OnClickListener {
     private String position;
     private String destFileName;//文件名 按照 妹子加当前页加索引
     private LikeView like,give;//点赞收藏
-    private ImageView dowonload,share;//下载分享
+    private ImageView dowonload,share,back,more;//下载分享
+    private Toolbar toolbar;
     private static final String destFileDir = "/storage/emulated/0/Android/data/com.yu.drysister/SisterImage";//下载后文件夹名称
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
-        showImage = findViewById(R.id.show_image);
-        loadingErr = findViewById(R.id.loading_err);
-        loadingErrText = findViewById(R.id.loading_err_text);
-        like = findViewById(R.id.like);
-        like.setOnClickListener(this);
-        give = findViewById(R.id.give);
-        give.setOnClickListener(this);
-        dowonload = findViewById(R.id.dowonload);
-        dowonload.setOnClickListener(this);
-        share = findViewById(R.id.share);
-        share.setOnClickListener(this);
+        initView();
+        initData();
+    }
 
+    private void initData() {
         url = getIntent().getStringExtra("url");
         page = getIntent().getStringExtra("page");
         position = getIntent().getStringExtra("position");
         load();
     }
 
+    private void initView() {
+        showImage = findViewById(R.id.show_image);
+        loadingErr = findViewById(R.id.loading_err);
+        loadingErrText = findViewById(R.id.loading_err_text);
+        toolbar = findViewById(R.id.toolbar);
+        like = findViewById(R.id.btn_like);
+        like.setOnClickListener(this);
+        give = findViewById(R.id.btn_give);
+        give.setOnClickListener(this);
+        dowonload = findViewById(R.id.btn_dowonload);
+        dowonload.setOnClickListener(this);
+        share = findViewById(R.id.btn_share);
+        share.setOnClickListener(this);
+        back = findViewById(R.id.btn_back);
+        back.setOnClickListener(this);
+        more = findViewById(R.id.btn_more);
+        more.setOnClickListener(this);
+    }
     //下载图片
     private void dwonloadImage() {
         destFileName = "妹子" + page + position + ".jpg";
@@ -138,16 +155,24 @@ public class ShowActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.like:
+            case R.id.btn_like:
                 like.toggle();
                 break;
-            case R.id.give:
+            case R.id.btn_give:
                 give.toggle();
                 break;
-            case R.id.dowonload:
+            case R.id.btn_dowonload:
                 dwonloadImage();
                 break;
-            case R.id.share:
+            case R.id.btn_share:
+                // TODO: 2019/7/2 分享按钮
+                ToastUtils.showShort("待实现");
+                break;
+            case R.id.btn_back:
+                 finish();
+                break;
+            case R.id.btn_more:
+                // TODO: 2019/7/3 更多按钮
                 ToastUtils.showShort("待实现");
                 break;
             default:
